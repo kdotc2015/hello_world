a1c00814f1db04556dadf2f83e74c7cae8ef66c9
Kyle <kylecozens@hotmail.com>
2015-08-03 11:29:43 +1000

    Initial Commit

---
+++ hello_world.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>//malloc
+#include<string.h>//
+
+
+
+
+
+        int main(int argc, char **argv){
+	
+	printf("Hello_world \n");
+	return 0;
+			        
+				        }  
